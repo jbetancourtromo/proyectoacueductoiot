2b97e3ca3fbdca1c2703128e0f48d440509f2885
jbetancourtromo <90102256+jbetancourtromo@users.noreply.github.com>
2025-09-18 10:07:44 -0500

    3_Temperatura_y_Humedad.py

--- pages/3_Temperatura_y_Humedad.py
+++ pages/3_Temperatura_y_Humedad.py
@@ -8,7 +8,7 @@ CHANNEL_ID = '3071480'
 READ_API_KEY = 'IHA53391H4BEBFJ7' # ¡Pega aquí tu Read API Key!
 URL_THINGSPEAK_LATEST = f'https://api.thingspeak.com/channels/{CHANNEL_ID}/feeds/last.json?api_key={READ_API_KEY}'
 
-st.title("💧 Sistema de Monitoreo del Acueducto - Samaniego")
+st.title("💧 Sistema de monitoreo acueducto - Samaniego")
 st.markdown("Programa Talento Tech 2025 - Proyecto Acueducto IoT -James Betancourt R -Christian Gaucales ")
 
 # --- Función para obtener los últimos datos ---
@@ -73,4 +73,5 @@ if sensor_data:
 else:
 
     st.warning("No se pudieron cargar los datos de temperatura y humedad.")
+
 
